14f15b3c2ee2d64a8af17213d9cf7c6e8dc1e94e
creativeeyestudio <contact@creative-eye.fr>
2025-05-13 20:05:55 +0200

    Access Page

---
+++ src/pages/acces-et-situation.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout/Layout"
+
+const AccessPage: React.FC = () => {
+    return (
+        <Layout noIntro={true}>
+            <section>
+                <h1>Accès et situation</h1>
+            </section>
+        </Layout>
+    )
+}
+
+export default AccessPage
