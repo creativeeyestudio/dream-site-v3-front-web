456816ad1eddf20cd9c5168e6b962dfc416093ff
creativeeyestudio <contact@creative-eye.fr>
2025-06-13 20:24:25 +0000

    Remove Hero Dependencies

--- src/components/ui/AccordionList.tsx
+++ src/components/ui/AccordionList.tsx
@@ -1,23 +1,8 @@
-import { Accordion, AccordionItem, SelectionMode } from "@heroui/react";
 import React from "react";
 
-export interface AccordionListProps {
-  items: string[];
-  multiple: SelectionMode;
-}
-
-const AccordionList: React.FC<AccordionListProps> = ({
-  items,
-  multiple = "none",
-}) => {
+const AccordionList: React.FC = () => {
   return (
-    <Accordion selectionMode={multiple}>
-      {items.map((item: string, index: number) => (
-        <AccordionItem key={index} aria-label="" title="" subtitle="">
-          {item}
-        </AccordionItem>
-      ))}
-    </Accordion>
+    <></>
   );
 };
 
--- src/components/ui/ModalBlock.tsx
+++ src/components/ui/ModalBlock.tsx
@@ -1,37 +1,7 @@
-import {
-  Button,
-  Modal,
-  ModalContent,
-  ModalBody,
-  useDisclosure,
-} from "@heroui/react";
-
-interface ModalBlockProps {
-  btnLabel: string;
-  size:
-    | "xs"
-    | "sm"
-    | "md"
-    | "lg"
-    | "xl"
-    | "2xl"
-    | "3xl"
-    | "4xl"
-    | "5xl"
-    | "full"
-    | undefined;
-}
-
-const ModalBlock: React.FC<ModalBlockProps> = ({ btnLabel, size = "5xl" }) => {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+const ModalBlock: React.FC = () => {
   return (
     <>
-      <Button onPress={onOpen}>{btnLabel}</Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} size={size}>
-        <ModalContent>
-          <ModalBody></ModalBody>
-        </ModalContent>
-      </Modal>
+      
     </>
   );
 };
