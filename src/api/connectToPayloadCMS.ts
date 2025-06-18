bb1111198a8e9eabee11c79214dabb4dcebec4d3
creativeeyestudio <contact@creative-eye.fr>
2025-06-18 21:13:50 +0000

    API Pages

--- src/api/connectToPayloadCMS.ts
+++ src/api/connectToPayloadCMS.ts
@@ -1,5 +1,5 @@
 export default async function connectToPayloadCMS() {
-    const user = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/login`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -7,16 +7,12 @@ export default async function connectToPayloadCMS() {
         body: JSON.stringify({
             email: process.env.NEXT_PUBLIC_API_USER,
             password: process.env.API_PASSWORD,
-        })
-    }).then(async (req) => await req.json())
-
-    const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
-        headers: {
-            Authorization: `JWT ${user.token}`,
-        }
+        }),
     })
 
-    if (!req.ok) throw new Error("Error during connect to Payload CMS");
-
-    return req;
+    if (!res.ok) throw new Error("Error during connect to Payload CMS");
+    
+    const { token } = await res.json();
+    
+    return token;
 }
--- src/api/pages.ts
+++ src/api/pages.ts
@@ -10,15 +10,20 @@ export async function getPage(locale: string, slug: string) {
 }
 
 async function initPage(locale: string, slug: string | null = null) {
-  connectToPayloadCMS();
+  const token = await connectToPayloadCMS();
   
   const apiSlug = slug
-    ? `pages?where[slug][equals]=${slug}&locale=${locale}`
-    : `pages?where[config.homepage][equals]=true&locale=${locale}`;
+    ? `?where[slug][equals]=${slug}&locale=${locale}`
+    : `?where[config.homepage][equals]=true&locale=${locale}`;
   
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/${apiSlug}`);
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/pages${apiSlug}`, {
+    headers: { Authorization: `JWT ${token}` }
+  });
   
-  if (!data.ok) notFound();
+  if (!data.ok) {
+    console.error(`Page not found with url : ${process.env.NEXT_PUBLIC_API_URL}/api/pages${apiSlug}`);
+    notFound()
+  };
   
   const page = await data.json();
   return page;
